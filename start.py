b2321bdc1e6966de4ae4061be9fadbd166d5440c
jholm117 <jholm117@gmail.com>
2017-04-24 21:09:38 -0500

    split into multiple files

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import random
-
+import Cards 
 
 # main update loop - not sure how its gonna work yet
 def Update():
@@ -12,54 +12,13 @@ def InitializeGame():
 	deck = []
 	for type in CardTypes:
 		for num in range (1,4):
-			deck.append(Card(type))
+			deck.append(Cards.Card(type))
 	
-	ShuffleDeck(deck)	
-	PrintDeck(deck)
+	Cards.ShuffleDeck(deck)	
+	Cards.PrintDeck(deck)
 	
 	return deck
 
-# rn just asks the player to press enter to shuffle, q to quit
-def PromptPlayer(deck):
-	s = raw_input("Press 'Enter' to shuffle -- 'q' to Quit\n")
-	if(s==''):
-
-		print "\nShuffling..."
-		ShuffleDeck(deck)
-		PrintDeck(deck)
-
-	elif (s == 'q'):
-		exit()
-
-	return
-
-# Shuffle cards in deck
-def ShuffleDeck(deck):
-	random.shuffle(deck)
-	return
-
-# Display types of cards in deck
-def PrintDeck(deck):
-	print "deck =\n"
-	for card in deck:
-		print card.get_type()
-	return
-
-# Card with type field
-class Card:
-	def __init__(self,kind):
-		self.type = kind
-
-	def get_type(self):
-		return self.type
-
-# Player object with hand field
-class Player:
-	def __init__(self,hand):
-		self.hand = hand
-
-	def get_hand(self):
-		return self.hand
 
 
 
@@ -68,7 +27,7 @@ class Player:
 def main():
 	deck = InitializeGame()
 	while(True):
-		PromptPlayer(deck)	
+		Cards.PromptPlayer(deck)	
 
 
 if __name__=="__main__":
