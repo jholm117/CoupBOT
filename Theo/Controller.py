e0817eedb0d81a62c8709e7cc97db25cf2c934ac
jholm117 <jholm117@gmail.com>
2017-04-27 18:45:11 -0500

    basic working version

--- Theo/Controller.py
+++ Theo/Controller.py
@@ -1,28 +1,26 @@
 # This is supposed to be a prototype for the different kinds of Controlleres i.e. AI bot, Learner Bot, or Human Controller
 # Will probably move the functions directly affecting game state to game.py
 class Action:
-	def __init__(self, n, ib, ic, it, ac=None, d=None, t=None):
+	def __init__(self, n, ib, ic, it, bb=None,ac=None):
 		self.name = n
-		self.target = t
-		self.doer = d
+		self.target = None
+		self.doer = None
 		self.isBlockable = ib
 		self.isChallengeable = ic
 		self.isTargetable = it
 		self.associatedCard = ac
+		self.blockableBy = bb
 
 characters = ['Duke', 'Assassin', 'Ambassador', 'Captain', 'Contessa']
 actions = [
 			Action('Income', False, False, False),
-			Action('Foreign Aid', True, False, False),
+			Action('Foreign Aid', True, False, False, ['Duke']),
 			Action('Coup', False, False, True),
-			Action('Tax', False, True, False, 'Duke'),
-			Action('Assassinate', True, True, True, 'Assassin'),
-			Action('Exchange', False, True, False, 'Ambassador'),
-			Action('Steal', True, True, True, 'Captain')
+			Action('Tax', False, True, False, None, 'Duke'),
+			Action('Assassinate', True, True, True, ['Contessa'], 'Assassin'),
+			Action('Exchange', False, True, False, None,'Ambassador'),
+			Action('Steal', True, True, True,['Captain','Ambassador'], 'Captain')
 			]
-#target_actions = ['Coup', 'Assassinate', 'Steal']
-counteractions = ['Block Foreign Aid', 'Block Stealing', 'Block Assassination']
-#responses = ['Allow', 'Counter', 'Challenge']
 
 class Controller:
 	def __init__(self, player):
@@ -38,25 +36,17 @@ class Controller:
 
 		if (response[0] == 'Challenge'):
 			#if you do have the card in your hand
-			self.RespondToChallenge(action,response[1],state)
+			self.RespondToChallenge(action.associatedCard,response[1],state)
 
-		elif (response[0] == 'Block'):
-			if self.DecideToChallenge():
-				#if successful
-				if self.Challenge():
-					state.DoAction(action);
-				else:
-					DecideCardToReveal()
-					RevealCard()
-
+		
 
 		elif(response[0] == 'Allow'):
 			state.DoAction(action)
-		
-		# debug info
-		else:
-			print 'unrecognized response'
-	
+			
+		# blocked -- response = card,blocker
+		elif not self.AnnounceBlock(state,action,response[0],response[1]):
+			state.DoAction(action)
+			
 		return
 
 
@@ -88,60 +78,76 @@ class Controller:
 		for player in players:
 			if (player == self.player):
 				continue
-			response = player.handler.DecideToCounter(action) 
+			response = player.handler.DecideToCounterAction(action) 
 			if response != 'Allow':
 				return response, player
 
 		return response, None
 
-	# Called when challenger challenges your action
-	def RespondToChallenge(self,action,challenger,state):
+	# returns whether Block is successful
+	def AnnounceBlock(self,state,action,card, blocker):
+		for player in state.players:
+			if player == blocker:
+				continue
+			else:				
+				if player.handler.DecideToChallengeBlock(action, blocker, card):
+					return blocker.handler.RespondToChallenge(card, player, state)
+		return True
+
+
+
+
+	# Called when challenger challenges self's action -- returns True if self was not lying
+	def RespondToChallenge(self, claimedCard, challenger, state):
 		for card in self.player.hand:
-			if action.associatedCard == card.name:
-				#Player wasn't lying
+			if claimedCard == card.name:
+				#Player wasn't lying -- Action successful
 				print challenger.name, ' incorrectly challenged ', self.player.name, ' !!!\n'
 				temp = challenger.handler.DecideCardToFlip()
 				state.challenger.RevealCard(challenger, temp)
 				state.ShuffleIntoDeck(self.player, [card])
 				state.Draw(self.player, 1)
-				state.DoAction(action)						# proceed with original action
-				return
+										
+				return True
 		
-		#player was lying
+		#player was lying -- Action unsuccessful
 		print challenger.name, ' correctly challenged ', self.player.name, ' !!!\n'
 		temp = self.player.handler.DecideCardToFlip()
 		state.RevealCard(self.player, temp)
-		return
+		return False		
 
-	# returns bool
-	def DecideToCounter(self,action):
+	def DecideToChallengeBlock(self, action, blocker, card):
+		print blocker.name, ' wants to block', action.doer.name,"'s ", action.name, ' with ', card
+		responses = ["Allow", "Challenge"]
+		
+		if DisplayOptions(responses,False) == 'Challenge':
+			return True
+		return False
+		
+
+	# returns Allow, Challenge, or Card to block with
+	def DecideToCounterAction(self,action):
 		print action.doer.name, ' wants to ', action.name
 		if action.target != None:
 			print 'TARGET == ', action.target.name
 		
 		responses = ['Allow']
 		if action.isBlockable:
-			if action.isTargetable and action.target == self.player:
+			if not action.isTargetable or action.target == self.player:				
 				responses.append('Block')
-			elif not action.isTargetable:
-				responses.append('Block')
-
+			
 		if action.isChallengeable:
 			responses.append('Challenge')
 
 		print 'How do you want to respond ', self.player.name, ' ??'
 		print '******'
 		response = DisplayOptions(responses, False)
-		print response
+		if response == 'Block':
+			print "Block With?"
+			response = DisplayOptions(action.blockableBy,False)
+
 		return response
 
-	# returns true if successful
-	def Challenge(self):
-		pass
-
-	#not sure what this would do 
-	def EndTurn(self):
-		return
 
 # isObj = true if object array
 def DisplayOptions(array, isObj, elementsToExclude=[]):
